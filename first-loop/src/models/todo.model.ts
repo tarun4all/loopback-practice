d50eb473524624334b3026c0a3b9a9c75efe2f0b
Tarun Bansal <tarun.bansal@sourcefuse.com>
2021-07-19 10:23:30 +0530

    todo added

---
+++ first-loop/src/models/todo.model.ts
@@ -0,0 +1,41 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+    required: true,
+  })
+  id: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  desc: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'boolean',
+    default: false,
+  })
+  isComplete?: boolean;
+
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
+
+export interface TodoRelations {
+  // describe navigational properties here
+}
+
+export type TodoWithRelations = Todo & TodoRelations;
